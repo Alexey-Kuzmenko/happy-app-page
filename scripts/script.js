d496c85546671565f0ccdd430bab9d9c2121de0f
Alexey Kuzmenko <alesha1103348@gmail.com>
2022-05-30 16:29:02 +0300

    added mobile menu icon and added header actions handler

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,31 @@
 (function () {
-    // alert('hello world')
+    // * UI elements
+    const body = document.body
+    const header = document.querySelector('.header')
+
+    // ! function calling
+    header.addEventListener('mouseover', onMouseOverHandler, false)
+    header.addEventListener('mouseout', onMouseOutHandler, false)
+    header.addEventListener('click', onClickHandler, false)
+
+    // * header handler
+    function onMouseOverHandler({ target }) {
+        if (target.classList.contains('menu__link')) {
+            target.classList.add('menu__link_active')
+        }
+    }
+
+    function onMouseOutHandler({ target }) {
+        if (target.classList.contains('menu__link')) {
+            target.classList.remove('menu__link_active')
+        }
+    }
+
+    // * menu icon handler 
+    function onClickHandler({ target }) {
+        if (target.classList.contains('header__menu-icon')) {
+            target.classList.toggle('header__menu-icon_active')
+        }
+    }
+
 })()
