9834fddc9b6b7b40db56141dd7b491ac9e5677fb
Alexey Kuzmenko <alesha1103348@gmail.com>
2022-05-31 17:59:38 +0300

    fixed bugs with mobile menu and navigation transition

--- scripts/script.js
+++ scripts/script.js
@@ -2,31 +2,27 @@
     // * UI elements
     const body = document.body
     const header = document.querySelector('.header')
+    const menu = document.querySelector('.menu')
+    const menuButtons = document.querySelectorAll('.menu__link')
 
     // ! function calling
-    header.addEventListener('mouseover', onMouseOverHandler, false)
-    header.addEventListener('mouseout', onMouseOutHandler, false)
     header.addEventListener('click', onClickHandler, false)
 
     // * header handler
-    function onMouseOverHandler({ target }) {
+    function onClickHandler({ target }) {
         if (target.classList.contains('menu__link')) {
             target.classList.add('menu__link_active')
-        }
-    }
 
-    function onMouseOutHandler({ target }) {
-        if (target.classList.contains('menu__link')) {
-            target.classList.remove('menu__link_active')
-        }
-    }
+            Array.from(menuButtons).forEach(link => {
+                if (link !== target) {
+                    link.classList.remove('menu__link_active')
+                }
+            })
 
-    // * menu icon handler 
-    function onClickHandler({ target }) {
-        if (target.classList.contains('header__menu-icon')) {
+        } else if (target.classList.contains('header__menu-icon')) {
             target.classList.toggle('header__menu-icon_active')
+            menu.classList.toggle('menu_active')
         }
     }
-
 
 })()
