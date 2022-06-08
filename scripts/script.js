b98b70bc389d50257a7bad299c5568c632ed7c93
Alexey Kuzmenko <alesha1103348@gmail.com>
2022-06-08 14:41:06 +0300

    added script for mobile menu

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@
     const header = document.querySelector('.header')
     const menu = document.querySelector('.menu')
     const menuButtons = document.querySelectorAll('.menu__link')
-    const blocks = document.querySelector('.main').children
+    const menuIcon = document.querySelector('.header__menu-icon')
     const toTopBtn = document.querySelector('.to-top-btn')
 
     // ! function calling
@@ -14,16 +14,12 @@
 
     // * header handler
     function onClickHandler({ target }) {
-        if (target.classList.contains('menu__link')) {
-            // target.classList.add('menu__link_active')
-
-            // Array.from(menuButtons).forEach(link => {
-            //     if (link !== target) {
-            //         link.classList.remove('menu__link_active')
-            //     }
-            // })
-
-        } else if (target.classList.contains('header__menu-icon')) {
+        if (target.classList.contains('menu__link') && menu.classList.contains('menu_active')) {
+            menu.classList.remove('menu_active')
+            menuIcon.classList.remove('header__menu-icon_active')
+            setBodyScroll(menuIcon)
+        }
+        else if (target.classList.contains('header__menu-icon')) {
             target.classList.toggle('header__menu-icon_active')
             menu.classList.toggle('menu_active')
             setBodyScroll(target)
