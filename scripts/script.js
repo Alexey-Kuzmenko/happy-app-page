7f292bfb9235390047682a7a595aaf7382099ca5
Alexey Kuzmenko <alesha1103348@gmail.com>
2022-06-08 10:54:17 +0300

    added to top button with functionality

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,12 @@
     const header = document.querySelector('.header')
     const menu = document.querySelector('.menu')
     const menuButtons = document.querySelectorAll('.menu__link')
+    const toTopBtn = document.querySelector('.to-top-btn')
 
     // ! function calling
     header.addEventListener('click', onClickHandler, false)
+    window.addEventListener('scroll', function () { userScroll() }, false)
+    toTopBtn.addEventListener('click', onToTopBtnClickHandler, false)
 
     // * header handler
     function onClickHandler({ target }) {
@@ -24,5 +27,23 @@
             menu.classList.toggle('menu_active')
         }
     }
+
+    // * render to top button
+    function userScroll() {
+        if (body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            toTopBtn.classList.add('to-top-btn_active')
+        } else {
+            toTopBtn.classList.remove('to-top-btn_active')
+        }
+    }
+
+    // * to top button handler
+    function onToTopBtnClickHandler(e) {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+
 
 })()
