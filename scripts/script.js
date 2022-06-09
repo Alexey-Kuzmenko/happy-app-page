f967202f54d6a7c0b25b4dcbbee96a5fcb8ab9c9
Alexey Kuzmenko <alesha1103348@gmail.com>
2022-06-09 11:06:07 +0300

    added cookies popup window

--- scripts/script.js
+++ scripts/script.js
@@ -6,11 +6,13 @@
     const menuButtons = document.querySelectorAll('.menu__link')
     const menuIcon = document.querySelector('.header__menu-icon')
     const toTopBtn = document.querySelector('.to-top-btn')
+    const pricingBlock = document.querySelector('.pricing-block')
 
     // ! function calling
     header.addEventListener('click', onClickHandler, false)
     window.addEventListener('scroll', function () { userScroll() }, false)
     toTopBtn.addEventListener('click', onToTopBtnClickHandler, false)
+    pricingBlock.addEventListener('click', pricingBlockHandler, false)
 
     // * header handler
     function onClickHandler({ target }) {
@@ -52,5 +54,104 @@
         })
     }
 
+    // * pricing block
+    function pricingBlockHandler(event) {
+        if (event.target.classList.contains('pricing-block__button')) {
+            event.preventDefault()
+        }
+    }
+
+    const popupContent = {
+        mainText: 'This website uses cookies in order to offer you the most relevant information. Please accept cookies for optimal performance.',
+        blueButtonText: 'Yes,accept cookies',
+        redButtonText: 'Ignore, and close',
+        alertButtontText: 'Ok'
+    }
+
+    // !popup template
+    function popupTamplate({ mainText, blueButtonText, redButtonText, alertButtontText }, type) {
+        let popupContentElements = []
+
+        // perent element
+        const popup = document.createElement('div')
+        popup.classList.add('popup')
+        popup.classList.add('popup_open')
+
+        // popup body
+        const popupBody = document.createElement('div')
+        popupBody.classList.add('popup__body')
+
+        // popup content
+        const popupContent = document.createElement('div')
+        popupContent.classList.add('popup__content')
+
+        //  popup text
+        const popupText = document.createElement('p')
+        popupText.classList.add('popup__text')
+        popupText.textContent = mainText
+        popupContentElements.push(popupText)
+
+        // popup close btn
+        const popupCloseBtn = document.createElement('img')
+        popupCloseBtn.classList.add('popup__close-btn')
+        popupCloseBtn.src = 'images/icons/x-circle.svg'
+        popupContentElements.push(popupCloseBtn)
+
+        // popup buttons
+        const popupButtons = document.createElement('div')
+        popupButtons.classList.add('popup__buttons')
+        popupContentElements.push(popupButtons)
+
+        if (type === 'cookies') {
+            const popupBlueButton = document.createElement('button')
+            popupBlueButton.classList.add('button')
+            popupBlueButton.classList.add('popup__button')
+            popupBlueButton.classList.add('popup__button_blue')
+            popupBlueButton.textContent = blueButtonText
+
+            const popupRedButton = document.createElement('button')
+            popupRedButton.classList.add('button')
+            popupRedButton.classList.add('popup__button')
+            popupRedButton.classList.add('popup__button_red')
+            popupRedButton.textContent = redButtonText
+
+            popupButtons.appendChild(popupBlueButton)
+            popupButtons.appendChild(popupRedButton)
+        }
+        else {
+            const popupBlueButton = document.createElement('button')
+            popupBlueButton.classList.add('button popup__button popup__button_blue')
+            popupBlueButton.textContent = alertButtontText
+
+            popupButtons.appendChild(popupBlueButton)
+        }
+
+        popupContentElements.forEach(node => {
+            popupContent.appendChild(node)
+        })
+
+        popupBody.appendChild(popupContent)
+        popup.appendChild(popupBody)
+
+        return popup
+    }
+
+    function renderPopupWindow(node = popupTamplate(popupContent, 'cookies')) {
+        const page = document.querySelector('.page')
+        page.insertAdjacentElement('afterend', node)
+        node.addEventListener('click', onPopupClickHandler, false)
+        body.dataset.bodyScroll = false
+    }
+
+    window.setTimeout(renderPopupWindow, 1000)
+
+    // * popup window handler
+    function onPopupClickHandler({ target }) {
+        if (target.classList.contains('popup__button') || target.classList.contains('popup__close-btn')) {
+            const popup = document.querySelector('.popup')
+            popup.classList.remove('popup_open')
+            body.dataset.bodyScroll = true
+        }
+    }
 
 })()
